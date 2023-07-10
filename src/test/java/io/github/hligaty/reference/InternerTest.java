bbd35cb2c4da2e629cf69a33d3a3a136911c13dc
hligaty <1970316213@qq.com>
2023-07-10 12:33:54 +0800

    Update InternerTest

--- src/test/java/io/github/hligaty/reference/InternerTest.java
+++ src/test/java/io/github/hligaty/reference/InternerTest.java
@@ -4,9 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.WeakHashMap;
 
 /**
  * @author hligaty
@@ -16,12 +14,7 @@ class InternerTest extends BaseTest {
     @Test
     public void test() {
         Interner<List<Integer>> interner = new Interner<>();
-        WeakHashMap<List<Integer>, Object> map = new WeakHashMap<>();
-        map.put(interner.intern(Arrays.asList(705, 630, 818)), new Object());
-        List<Integer> weakObject = interner.intern(Collections.singletonList(705630818));
-        map.put(weakObject, new Object());
-        gc();
-        Assertions.assertNull(map.get(interner.intern(Arrays.asList(705, 630, 818))));
-        Assertions.assertNotNull(map.get(interner.intern(Collections.singletonList(705630818))));
+        List<Integer> intern = interner.intern(Arrays.asList(705, 630, 818));
+        Assertions.assertSame(intern, interner.intern(Arrays.asList(705, 630, 818)));
     }
 }
